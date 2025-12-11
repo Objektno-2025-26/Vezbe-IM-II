79f704a1625c7beef15382396e6f62debcc34a0b
VladimirFabri98 <vfficial98@gmail.com>
2025-12-11 15:31:14 +0100

    Vezbe 5 - 11.12.2025.

--- Projekat/src/geometry/Main.java
+++ Projekat/src/geometry/Main.java
@@ -3,6 +3,7 @@ package geometry;
 public class Main {
 
 	public static void main(String[] args) {
+		/*
 //		Primitivni tip podatka - value type
 		int broj = 20;
 //		Slozeni tip podatka - reference type
@@ -30,6 +31,45 @@ public class Main {
 		p1.setY(99); 
 		System.out.println(p2.getY());
 		System.out.println(p1.getY());
+		*/
+		
+//		Vezbe 5
+		Point p1 = new Point();
+		Point p2 = new Point();
+		Line l1 = new Line();
+		System.out.println(l1.getStartPoint());
+		System.out.println(l1.getEndPoint());
+//		Atributu startPoint se dodeljuje referenca p1, nakon cega te dve reference pokazuju
+//		na isti objekat. Ista prica je i za endPoint i p2
+		l1.setStartPoint(p1);
+		l1.setEndPoint(p2);
+		
+//		startPoint i p1 sada pokazuju na isti objekat i to je vidljivo u ispisu na konzoli
+		System.out.println(l1.getStartPoint());
+		System.out.println(p1);
+		
+//		Promena vrednosti x ili y vrednosti tog objekta je moguca kroz i jednu i drugu referencu
+//		Sustinski, u ovom trenutku, p1 i startPoint pokazuju na isti objekat i getX() i getY() kroz
+//		bilo koju referencu ce uvek vratiti isti rezultat
+		p1.setX(50);
+		System.out.println(l1.getStartPoint().getX());
+		l1.getStartPoint().setY(20);
+		System.out.println(p1.getY());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 
 	}
